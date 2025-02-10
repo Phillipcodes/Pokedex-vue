dd57632e849604d6b484d341898dba944315d9a4
Phillipcodes <Phillipsauer00@gmail.com>
2025-02-10 15:39:08 +0100

    first styling with tailwind

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template lang="">
-    <header>
-        <span>Pokedex</span>
+    <header class="bg-red-600 header flex items-center">
+        <span class="font-bold text-[32px] tracking-wider pl-16"  >Pokedex</span>
     </header>
 </template>
 <script>
@@ -8,6 +8,12 @@ export default {
     name:'HeaderComponent'
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+    .header {
+        width: 100%;
+        height: 100px;
+        border-bottom-right-radius: 8px;
+        border-bottom-left-radius: 8px;
+        box-sizing: border-box;
+    }
 </style>
