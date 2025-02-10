9dd2b95531905c9724a5ef39dce094059cf67ac6
Phillipcodes <Phillipsauer00@gmail.com>
2025-02-10 16:50:58 +0100

    created a clip path tag

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template lang="">
     
    <HeaderComponent></HeaderComponent>
+<div class="tag"></div>
+
 </template>
 <script>
 import HeaderComponent from './components/Header.vue';
@@ -16,5 +18,13 @@ export default {
         margin: 0;
         background: #2B292C;
         color: white;
+        font-family: 'Helvetica';
     }
+    .tag {
+        clip-path: polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%);
+        background-color: #FFFAFA;
+        height: 30px;
+        width: 55px;
+    }
+    
 </style>
